3530a163f5b24e8f8971967a54f16cb29f3a0e38
Andres Quesada <andresquesada71@gmail.com>
2022-06-16 12:58:28 -0600

    16 de julio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Embed from './components/Embed';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Basic Embed</h1>
+      <Embed />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Embed.js
@@ -0,0 +1,84 @@
+import React, { useRef, useEffect } from "react";
+import styles from "./Embed.module.css";
+
+const { tableau } = window;
+
+function Embed() {
+    var viz;
+  const ref = useRef(null);
+  const url =
+    "https://public.tableau.com/views/Ingresospersonales2001-2013/Dashboard1";
+
+  const options = {
+    device: "desktop",
+    hideTabs: true,
+  };
+
+  const initViz = () => {
+    viz = new tableau.Viz(ref.current, url, options);
+  };
+
+  useEffect(() => {
+    initViz();
+  }, []);
+
+  // Opens the Download to PDF dialog box
+  function exportToPDF() {
+    viz.showExportPDFDialog();
+  }
+
+  // Downloads the crosstab data in an Excel file
+  function exportToExcel() {
+    viz.exportCrossTabToExcel();
+  }
+
+  // Opens the Download Image dialog box
+  function exportImage() {
+    viz.showExportImageDialog();
+  }
+
+  function exportPowerPoint() {
+    viz.showExportPowerPointDialog();
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.graphics} ref={ref} />
+      <div className={styles.download}>
+        <h1>Descargar</h1>
+        <div className={styles.download_options}>
+          <button onClick={exportToPDF}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/179/179483.png"
+              alt=""
+            ></img>
+            PDF
+          </button>
+          <button onClick={exportImage}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/2659/2659360.png"
+              alt=""
+            ></img>
+            IMG
+          </button>
+          <button onClick={exportToExcel}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/888/888850.png"
+              alt=""
+            ></img>
+            EXCEL
+          </button>
+          <button onClick={exportPowerPoint}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/888/888874.png"
+              alt=""
+            ></img>
+            PPT
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Embed;
