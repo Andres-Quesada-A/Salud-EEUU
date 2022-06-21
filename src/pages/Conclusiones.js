bfd6755ffa1fb02305b45728c33996f4cd68d1e5
Andres Quesada <andresquesada71@gmail.com>
2022-06-20 23:43:44 -0600

    conclusiones

--- src/pages/Conclusiones.js
+++ src/pages/Conclusiones.js
@@ -4,68 +4,7 @@ export default function Conclusiones() {
   return (
     <div className="conclusiones">
       <h1>Conclusiones</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada
-        fames ac turpis egestas maecenas pharetra. Vel fringilla est ullamcorper
-        eget nulla facilisi etiam dignissim. Viverra adipiscing at in tellus
-        integer. Amet venenatis urna cursus eget nunc scelerisque viverra
-        mauris. Adipiscing elit duis tristique sollicitudin nibh sit amet.
-        Pellentesque id nibh tortor id aliquet lectus proin. Parturient montes
-        nascetur ridiculus mus mauris vitae ultricies leo. Nec ultrices dui
-        sapien eget mi proin sed. Odio euismod lacinia at quis risus sed
-        vulputate. Lorem donec massa sapien faucibus et molestie ac feugiat.
-        Rutrum quisque non tellus orci. Aliquam faucibus purus in massa tempor
-        nec feugiat. Sed odio morbi quis commodo odio. Commodo viverra maecenas
-        accumsan lacus vel facilisis volutpat est velit. Sed nisi lacus sed
-        viverra. Nec sagittis aliquam malesuada bibendum arcu. Pellentesque id
-        nibh tortor id aliquet lectus proin.
-      </p>
-      <p>
-        Consequat interdum varius sit amet. Sem et tortor consequat id porta
-        nibh. Ridiculus mus mauris vitae ultricies leo integer malesuada nunc.
-        Id consectetur purus ut faucibus pulvinar elementum integer enim neque.
-        Leo vel orci porta non pulvinar neque laoreet. Est velit egestas dui id
-        ornare arcu odio ut. Nisl purus in mollis nunc sed id semper risus.
-        Donec adipiscing tristique risus nec feugiat in fermentum posuere. In
-        fermentum posuere urna nec tincidunt praesent. Enim eu turpis egestas
-        pretium aenean pharetra. Euismod lacinia at quis risus. Viverra aliquet
-        eget sit amet tellus. Sit amet mauris commodo quis imperdiet massa
-        tincidunt nunc pulvinar. Convallis aenean et tortor at risus.
-        Condimentum id venenatis a condimentum vitae. Amet est placerat in
-        egestas erat imperdiet sed euismod. In nulla posuere sollicitudin
-        aliquam ultrices.
-      </p>
-
-      <p>
-        Feugiat vivamus at augue eget arcu dictum varius duis at. Tortor id
-        aliquet lectus proin. Neque viverra justo nec ultrices. Ipsum nunc
-        aliquet bibendum enim. In pellentesque massa placerat duis ultricies
-        lacus sed turpis tincidunt. Odio ut sem nulla pharetra diam sit amet
-        nisl. Bibendum enim facilisis gravida neque convallis a cras. Aliquam
-        purus sit amet luctus. Faucibus pulvinar elementum integer enim neque
-        volutpat. Ante metus dictum at tempor commodo ullamcorper. Sit amet nisl
-        suscipit adipiscing bibendum est.
-      </p>
-      <p>
-        Nulla aliquet porttitor lacus luctus accumsan tortor. Viverra mauris in
-        aliquam sem. Commodo sed egestas egestas fringilla phasellus faucibus
-        scelerisque. Scelerisque varius morbi enim nunc faucibus a pellentesque
-        sit. Enim eu turpis egestas pretium. Odio euismod lacinia at quis risus
-        sed vulputate. Enim ut tellus elementum sagittis vitae et. Ultricies leo
-        integer malesuada nunc vel. Faucibus scelerisque eleifend donec pretium
-        vulputate sapien nec sagittis aliquam. Convallis tellus id interdum
-        velit laoreet id.
-      </p>
-      <p>
-        Ut sem viverra aliquet eget sit amet tellus cras. Tellus elementum
-        sagittis vitae et leo. Mattis aliquam faucibus purus in. Auctor urna
-        nunc id cursus metus aliquam. Tellus elementum sagittis vitae et leo
-        duis. Phasellus vestibulum lorem sed risus ultricies tristique nulla
-        aliquet enim. Nisi lacus sed viverra tellus. Elementum nisi quis
-        eleifend quam adipiscing. Fusce id velit ut tortor pretium viverra. Nunc
-        faucibus a pellentesque sit.
-      </p>
+      <p>Durante el desarrollo de este proyecto, logramos encontrar una cantidad variada de de información que nos ayudaron a entender de una mejor manera las relaciones existentes entre el estado de salud y la mortalidad de los Estadounidenses, así como percibir de forma más detallada los factores que influyen en estos. Vimos que mientras más ingresos tienen las personas, es más probable que las personas tengan una mejor calidad de vida, es decir mayor salud y menor probabilidad de enfermedad.</p>
     </div>
   );
 }
